530a42a02fe9e0e08b380ca6ce0bb52a0402318a
Rifaldi01 <rifaldipratama2004@gmail.com>
2021-02-05 07:29:19 -0800

    frist commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layout/main')
+ 
+ @section('title', 'About')
+
+
+ @section('container')
+    <div class="container">
+        <div class="row">
+           <div class="col-10">
+                <h1 class="mt-3">Tentang RIFALDI </h1>
+                <div class="container">
+    <div class="list-group">
+  <a class="list-group-item list-group-item-action active" aria-current="true">
+    INFO RIFALDI
+  </a>
+  <a  class="list-group-item list-group-item-action">Bandung, 1 April 2004</a>
+  <a class="list-group-item list-group-item-action">Kp. Leuwidulang des. Rancamulya</a>
+  <a class="list-group-item list-group-item-action">Pelajar</a>
+  <a class="list-group-item list-group-item-action ">SMK MAHAPUTRA CERDAS UTAMA</a>
+</div>
+</div>
+           </div>
+        </div>                                                    
+    </div>
+    @endsection
+
+
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main');
+ 
+ @section('title', 'Tugas PPL');
+
+
+ @section('container')
+    <div class="container">
+        <div class="row">
+           <div class="col-10">
+               <center> <h1 class="mt-5">RIFALDI SATYA PRATAMA</h1></center>
+           </div>
+        </div>                                                    
+    </div>
+    @endsection
